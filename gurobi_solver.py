c599885f8120b4836f9fdace96751dbd3bd67a2e
jtirana98 <joana.tirana98@gmail.com>
2023-05-06 00:13:03 +0200

    fwd only with gurobi solver

---
+++ gurobi_solver.py
@@ -0,0 +1,96 @@
+import numpy as np
+
+import gurobipy as gp
+from gurobipy import GRB
+from gurobipy import quicksum as qsum
+
+import utils
+import time
+
+import warnings
+warnings.filterwarnings("ignore")
+
+def main():
+    K = 200 # number of data owners
+    H = 5 # number of compute nodes
+    utils.file_name = 'fully_symmetric.xlsx'
+
+    release_date = np.array(utils.get_fwd_release_delays(K,H))
+    memory_capacity = np.array(utils.get_memory_characteristics(H, K))
+    proc = np.array(utils.get_fwd_proc_compute_node(K, H))
+    proc_local = np.array(utils.get_fwd_end_local(K))
+    trans_back = np.array(utils.get_trans_back(K, H))
+
+    T = np.max(release_date) + K*np.max(proc[0,:]) # time intervals
+    print(f"T = {T}")
+
+    ones_H = np.ones((H,1))
+    ones_K = np.ones((K,1))
+    ones_T = np.ones((T,1))
+
+    m = gp.Model("mip1")
+
+    # define variables
+    x = m.addMVar(shape = (H,K,T), vtype=GRB.BINARY, name="x")
+    y = m.addMVar(shape=(K,H), vtype=GRB.BINARY, name="y")
+    f = m.addMVar(shape=(K), vtype=GRB.INTEGER, name="f")
+    maxobj = m.addMVar(shape=(1),vtype=GRB.INTEGER, name="maxobj")
+    comp = m.addMVar(shape=(K),vtype=GRB.INTEGER, name="comp")
+
+
+    start = time.time()
+    # define constraints
+    # C1: job cannot be assigned to a time interval before the release time
+    for i in range(H): #for all devices
+        for j in range(K): #for all jobs
+            for t in range(T): #for all timeslots
+                if t < release_date[j,i]:
+                    m.addConstr(x[i,j,t] == 0)
+
+    # C3: all jobs interval are assigned to one only machine
+    m.addConstr( y @ ones_H == ones_K )
+    
+    # C4: memory constraint
+    m.addConstr((y.T * utils.max_memory_demand) @ ones_K <= memory_capacity.reshape(ones_H.shape))
+    
+    # C6: machine processes only a single job at each interval
+    for j in range(H): #for all devices
+        m.addConstr( x[j,:,:].T @ ones_K <= ones_T )
+    
+    # C9: new constraint - the merge of C2 and C3 (job should be process all once and only in one machine)
+    for j in range(H): #for all machines
+        for i in range(K):
+            m.addConstr( qsum(x[j,i,:]) == y[i,j]*proc[i,j])
+
+    for j in range(H): #for all machines
+        for i in range(K):
+            m.addConstrs( f[i] >= (t+1)*x[j,i,t] for t in range(T))
+
+    # Define objective function
+    
+
+    m.addConstrs(comp[i] == qsum(trans_back[i,:] * y[i,:]) + f[i] + proc_local[i] for i in range(K))
+       
+    
+    max_constr = m.addConstr(maxobj == gp.max_(comp[i] for i in range(K)))
+    
+    m.setObjective(maxobj, GRB.MINIMIZE)
+    end = time.time()
+    time1 = end-start
+    print(f'problem formulation: {time1}')
+
+
+    start = time.time()
+    # Optimize model
+    m.optimize()
+    end = time.time()
+    print(f'problem solver: {end-start}')
+    print(f'TOTAL: {(end-start) + time1}')
+    #for v in m.getVars():
+    #    print('%s %g' % (v.VarName, v.X))
+
+    print('Obj: %g' % m.ObjVal)
+
+
+if __name__ == '__main__':
+    main()
