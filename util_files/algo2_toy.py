ce56c603c9aeefd881cf401a10df6b5f4fb5cc62
jtirana98 <joana.tirana98@gmail.com>
2024-01-24 13:58:00 +0000

    implementation of toy algo2

--- util_files/algo2_toy.py
+++ util_files/algo2_toy.py
@@ -28,7 +28,7 @@ def main():
     ll = np.array([5, 3, 8, 1, 1]) # transmission an proc at the client
     duration = sum(arriv) + sum(proc)
 
-    allocation = np.zeros((duration))
+    allocation = np.zeros((duration)) # if allocation[i] = 0 there is no task allocated, if allocation = k, k>0 then client k has been allocated at machine
     # step - 1 order clients by releasing date
     order = np.argsort(arriv)
     print(order)
@@ -95,103 +95,147 @@ def main():
         if len(B[bita][1]) == 1: # no need to reschedule
             continue
         
-        bloc = B[bita]
-        print(bloc[0][1])
-        # step - 3 find l-client
-        l = -1
-        min = duration+100
-        for client in bloc[1]:
-            temp = bloc[0][1]+ll[int(client)-1]
-            if temp < min:
-                min = temp
-                l = client
-        print(f'l client is {l}')
-
-        # step 4 - reschedule
-        # l tasks should allocated in slots where no other client has been released
-
-        # find start of l
-        start_l = -1
-        for i in range(bloc[0][0], bloc[0][1]):
-            if allocation[i] == l:
-                start_l = i
-                break
+        print(f'Starting exploring new MAIN block')
         
-        allocated_slots = 0
-        #for i in range(ss, bloc[0][1]):
-        no_change = True
-        i = start_l
-        while True:
-            give_priority = -1
-            for j in range(num_clients):
-                client_c = order[j]
-                if client_c + 1 == l:
-                    continue
-                if arriv[client_c] <= i and client_c+1 in bloc[1]:
-                    print(f'checking {client_c}')
-                    is_allocated = False
-                    for ii in range(bloc[0][0], i):
-                        if allocation[ii] == client_c+1:
-                            is_allocated = True
-                            break
-                    if is_allocated:
-                        print('ignore')
-                        continue
-                    else:
-                        print('this is the one')
-                        give_priority = client_c+1
-                        break
-            if give_priority == -1:
-                allocated_slots += 1
-                allocation[i] = l
-                i += 1
-                if allocated_slots == proc[int(l)-1]:
-                    break
-            else:
-                for j in range(proc[give_priority-1]): # shift client
-                    allocation[i+j] = give_priority
-                i = i +  proc[give_priority-1]
-                no_change = False
+        sub_blocks = [B[bita]]
+        block_i = 0
+        while len(sub_blocks) > 0: 
+            bloc = sub_blocks.pop(block_i)
+            print(f'Starting exploring new sub-block block: start-{bloc[0][0]} end-{bloc[0][1]}  clients {bloc[1]}')
+            if len(bloc[1]) == 1:
+                print('skip block, has only one client')
+                if len(sub_blocks) != 0:
+                        block_i = (block_i + 1)%len(sub_blocks)
+                continue
             
+            
+
+            # step - 3 find l-client
+            l = -1
+            min = duration+100
+            for client in bloc[1]:
+                temp = bloc[0][1]+ll[int(client)-1]
+                if temp < min:
+                    min = temp
+                    l = client
+            print(f'l-client is {l}')
+
+            # step 4 - reschedule
+            # l tasks should allocated in slots where no other client has been released
+
+            # find start of l
+            start_l = -1
+            for i in range(bloc[0][0], bloc[0][1]):
+                if allocation[i] == l:
+                    start_l = i
+                    break
+            
+            allocated_slots = 0
+            
+            slot_i = start_l
+            at_least_one = False
+            while slot_i <= bloc[0][1]:
+                no_change = True
+                for jj in range(num_clients):
+                    give_priority = -1
+                    client_c = order[jj]
+                    if client_c + 1 == l:
+                        continue
+                    #print(f'!!! {client_c+1} {slot_i} {arriv[client_c]}')
+                    if arriv[client_c] <= slot_i and (client_c+1 in bloc[1]):
+                        #print(f'checking {client_c+1}')
+                        is_allocated = False
+                        ii = bloc[0][0]
+                        while ii <= slot_i:
+                            if allocation[ii] == client_c+1:
+                                is_allocated = True
+                            ii += 1
+                        
+                        if is_allocated:
+                            #print('ignore')
+                            continue
+                        
+                        #print('this is the one')
+                        give_priority = client_c+1
+
+                    #print(give_priority)
+                    if give_priority != -1:
+                        #print('here2')
+                        for j in range(proc[give_priority-1]): # shift client
+                            allocation[slot_i+j] = give_priority
+                        slot_i = slot_i +  proc[give_priority-1]
+                        no_change = False
+                        at_least_one = True
+                        break
+                    
+                if no_change:
+                    if allocated_slots == 0:
+                        start_l = slot_i
+                    allocated_slots += 1
+                    allocation[slot_i] = l
+                    slot_i += 1
+                    if allocated_slots == proc[int(l)-1]:
+                        break
+                        
+
             #if cannot reschedule:
-            if no_change:
+            if not at_least_one:
                 print('block cannot be updated any more')
+                if len(sub_blocks) != 0:
+                    block_i = (block_i + 1)%len(sub_blocks)
+                continue
+            
+            print('New scheduling')
+            print_res(allocation, duration, num_clients)
+            
+            # step 5 - update subset
+            slot_i = bloc[0][0]
+            
+            new_block_f = True
+            print(bloc[0][1])
+            while slot_i < bloc[0][1]:
+                #print(f'In {slot_i}')
+                if new_block_f:
+                    #print('new block')
+                    new_block = [[slot_i,-1],[]]
+                    new_block_f = False
+                
+                if allocation[slot_i] == l:
+                    #print('skip')
+                    if len(new_block[1]) > 0:
+                        new_block[0][1] = slot_i
+                        #print('added block')
+                        sub_blocks.append(new_block)
+                    
+                    new_block_f = True
+                    slot_i += 1
+                    continue
+                    #while allocation[slot_i] == l:
+                    #slot_i += 1
 
-    print('After one iteraion')
-    print_res(allocation, duration, num_clients)
-    
-    # step 5 - update subset
-    # remove subset[i] from subset
-    # ftiakse nea subset thewrontas ton l idle
+                elif allocation[slot_i] not in new_block[1]:
+                    #print('added client')
+                    new_block[1].append(allocation[slot_i])
+                
+                new_block[0][1] = slot_i
+                if slot_i == bloc[0][1]-1:
+                    #print('added block')
+                    sub_blocks.append(new_block)
+                slot_i += 1
+            
 
-    '''
-    for bita in list(B.keys()):
-        if len(B[bita]) == 1:
-            continue
-        subset = [bita]
-        fixed = 0 # number of sub-blocks that cannot change any more
-        while fixed < len(subset):
-            for i in range(len(subset)):
-                if len(subset[i]) == 1:
-                    fixed += 1
+            print(f'Remaining blocks: {len(sub_blocks)}')
+            if len(sub_blocks) != 0:
+                block_i = (block_i + 1)%len(sub_blocks)
 
-                # step - 3 find l-client
-                l = 0
+            
 
-                # step 4 - reschedule
-
-                #if cannot reschedule:
-                fixed += 1
-
-                # step 5 - update subset
-                # remove subset[i] from subset
-                # ftiakse nea subset thewrontas ton l idle
-                subset.append()
-    '''
-    
+   
     # show result
     makespan = 0
     finish_clients = [0 for i in range(num_clients)]
+    print('FINAL scheduling')
+    print_res(allocation, duration, num_clients)
 
 
 if __name__ == '__main__':
