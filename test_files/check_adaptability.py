1d2c7d80fb69b66dbbb050ed1171465ed3c3af1a
Joana Tirana <joana.tirana98@gmail.com>
2024-11-02 21:39:41 +0100

    first step for the adaptation analysis

--- test_files/check_adaptability.py
+++ test_files/check_adaptability.py
@@ -7,17 +7,18 @@ sys.path.insert(0,'../util_files')
 
 import ILP_hybrid as ilp_hybrid
 import ADMM_hybrid as admm_hybrid
+import heuristic_FCFS as fcfs_sol
 import utils as utils
 
 
 if __name__ == '__main__':
     logs = 'test1.txt'
-    K = 5
-    H = 2
+    K = 30
+    H = 5
 
     scenario = 1 # low heterogeneity 
 
-    splitting_points = '20,30'
+    splitting_points = '2,30'
 
     points = list(splitting_points.split(','))
     point_a = int(points[0])
@@ -46,8 +47,8 @@ if __name__ == '__main__':
     proc_local, trans_back, 
     memory_capacity, memory_demand, 
     release_date_back, proc_bck, 
-    proc_local_back, trans_back_gradients) = utils.create_scenario_hybrid_scenario2(filename, point_a, point_b, 
-                                                                                K, H, 5000, scenario)
+    proc_local_back, trans_back_gradients) = utils.create_scenario_hybrid(filename, point_a, point_b, 
+                                                                                K, H, 6000, scenario)
 
     # Define the time horizon
     T_hybrid = np.max(release_date[1]) + int(K/H)*np.max(proc[1][0,0:H]) + np.max([proc[1][k,H+k] for k in range(K)])  \
@@ -56,6 +57,7 @@ if __name__ == '__main__':
                         + np.max(np.max(trans_back[1])) + np.max(np.max(trans_back_gradients[1])) 
 
     T = int(T_hybrid)
+    print(T)
 
     w_hybrid_admm = ([0,0], -1)
     print('---------------------- ADMM -----------------------------------')
@@ -65,6 +67,13 @@ if __name__ == '__main__':
                                             release_date_back[1].astype(int), proc_bck[1].astype(int), 
                                             proc_local_back[1].astype(int), trans_back_gradients[1].astype(int))
     
+    w_fcfs = fcfs_sol.run_hybrid(K, H, release_date[1].astype(int), proc[1].astype(int), 
+                                            proc_local[1].astype(int), trans_back[1].astype(int), 
+                                            memory_capacity[1].astype(int), [memory_demand[1].astype(int) for i in range(K)],
+                                            release_date_back[1].astype(int), proc_bck[1].astype(int), 
+                                            proc_local_back[1].astype(int), trans_back_gradients[1].astype(int))
+    
 
     
     print(f"{utils.bcolors.OKGREEN}The hybrid-makespan for the admm is {w_hybrid_admm[0][-1]}{utils.bcolors.ENDC}")
+    print(f"{utils.bcolors.OKGREEN}The makespan for FCFS is  {w_fcfs}{utils.bcolors.ENDC}")  
